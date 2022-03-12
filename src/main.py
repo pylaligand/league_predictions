5eb77e2bf7270c33ea28d436cf3a828af619fc1c
P.Y. Laligand <py.laligand@gmail.com>
2022-03-12 18:38:35 +0100

    ELO-based engine

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 
 import argparse
 
+from algo import GameSimulator
+from elo import EloEngine
 from parsing import load_season
 
 
@@ -14,7 +16,14 @@ def main():
         lines = [l.strip() for l in input_file.readlines()]
 
     season = load_season(lines)
-    print(season)
+    engine = EloEngine(start=1000, k=20, min=100)
+    simulator = GameSimulator(season, engine)
+    simulator.simulate()
+    ranked_teams = sorted(simulator.teams(),
+                          key=lambda t: t.points,
+                          reverse=True)
+    for t in ranked_teams:
+        print('{:10s} {:3d}'.format(t.name, t.points))
 
 
 if __name__ == "__main__":
---
+++ src/test_elo.py
@@ -0,0 +1,49 @@
+import unittest
+
+from algo import GameSimulator
+from elo import EloEngine
+from parsing import load_season
+
+
+class TestElgo(unittest.TestCase):
+    def test_one_gameday(self):
+        data = [
+            'day1,,,',
+            'A,1,B,2',
+        ]
+        season = load_season(data, validate=False)
+        engine = EloEngine(start=1000, k=20, min=100)
+        simulator = GameSimulator(season, engine)
+        simulator.simulate()
+        self.assertEquals(simulator.get_team('A').rating, 990)
+        self.assertEquals(simulator.get_team('B').rating, 1010)
+
+    def test_two_gamedays(self):
+        data = [
+            'day1,,,',
+            'A,1,B,2',
+            'day2,,,',
+            'B,3,A,1',
+        ]
+        season = load_season(data, validate=False)
+        engine = EloEngine(start=1000, k=20, min=100)
+        simulator = GameSimulator(season, engine)
+        simulator.simulate()
+        self.assertEquals(simulator.get_team('A').rating, 980)
+        self.assertEquals(simulator.get_team('B').rating, 1019)
+
+    def test_three_gamedays(self):
+        data = [
+            'day1,,,',
+            'A,1,B,2',
+            'day2,,,',
+            'B,3,A,1',
+            'day3,,,',
+            'A,4,B,0',
+        ]
+        season = load_season(data, validate=False)
+        engine = EloEngine(start=1000, k=20, min=100)
+        simulator = GameSimulator(season, engine)
+        simulator.simulate()
+        self.assertEquals(simulator.get_team('A').rating, 991)
+        self.assertEquals(simulator.get_team('B').rating, 1007)
